cc1e1117d507f4b6aaae0fa9bfbec0f5cdb81998
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2026-04-22 14:55:27 +0100

    python: add type annotation to tests

--- libraries/python/tests/test_mnemonyms.py
+++ libraries/python/tests/test_mnemonyms.py
@@ -7,26 +7,27 @@ from mnemonyms import Mnemonym
 class MnemonymTest(unittest.TestCase):
     def test_vectors(self) -> None:
         with open("../../../test-vectors.json", "r") as fil:
-            vectors = json.load(fil)
+            vectors: dict[str, str] = json.load(fil)
 
         for lang in vectors.keys():
-            path = os.path.join(
+            path: str = os.path.join(
                 os.path.dirname(__file__),
                 f"../../../wordlists/{lang}.txt",
             )
-            wordlist = []
+            wordlist: list[str] = []
 
             if os.path.exists(path) and os.path.isfile(path):
                 with open(path, "r", encoding="utf-8") as f:
-                    wordlist = [w.strip() for w in f.readlines()]
+                    wordlist: list[str] = [w.strip() for w in f.readlines()]
             else:
                 raise EnvironmentError(f"Wordlist for {lang} not detected")
 
             for vec in vectors[lang]:
-                strength = len(bytes.fromhex(vec[0])) * 8
-                nym = Mnemonym(False, strength, wordlist)
-                nym_from_eny = nym.to_nym(bytes.fromhex(vec[0]))
-                eny_from_nym = nym.to_eny(nym_from_eny)
+                strength: int = len(bytes.fromhex(vec[0])) * 8
+                nym: Mnemonym = Mnemonym(False, strength, wordlist)
+                nym_from_eny: str = nym.to_nym(bytes.fromhex(vec[0]))
+                eny_from_nym: str = nym.to_eny(nym_from_eny)
+
                 self.assertEqual(
                     vec[0], eny_from_nym, f"round-trip failed for entropy {vec[0]}"
                 )
@@ -36,22 +37,22 @@ class MnemonymTest(unittest.TestCase):
 
     def test_complete_word(self) -> None:
         with open("../../../test-vectors.json", "r") as fil:
-            vectors = json.load(fil)
+            vectors: dict[str, str] = json.load(fil)
 
         for lang in vectors.keys():
-            path = os.path.join(
+            path: str = os.path.join(
                 os.path.dirname(__file__),
                 f"../../../wordlists/{lang}.txt",
             )
-            wordlist = []
+            wordlist: list[str] = []
 
             if os.path.exists(path) and os.path.isfile(path):
                 with open(path, "r", encoding="utf-8") as f:
-                    wordlist = [w.strip() for w in f.readlines()]
+                    wordlist: list[str] = [w.strip() for w in f.readlines()]
             else:
                 raise EnvironmentError(f"Wordlist for {lang} not detected")
 
-        nym = Mnemonym(False, 256, wordlist)
+        nym: Mnemonym = Mnemonym(False, 256, wordlist)
 
         self.assertEqual("zone", nym.complete_word(
             "..zoo.zoo.zoo.zoo.zoo.zoo.zoo.zoo.zoo.zon"))
@@ -62,22 +63,22 @@ class MnemonymTest(unittest.TestCase):
 
     def test_complete_nym(self) -> None:
         with open("../../../test-vectors.json", "r") as fil:
-            vectors = json.load(fil)
+            vectors: dict[str, str] = json.load(fil)
 
         for lang in vectors.keys():
-            path = os.path.join(
+            path: str = os.path.join(
                 os.path.dirname(__file__),
                 f"../../../wordlists/{lang}.txt",
             )
-            wordlist = []
+            wordlist: list[str] = []
 
             if os.path.exists(path) and os.path.isfile(path):
                 with open(path, "r", encoding="utf-8") as f:
-                    wordlist = [w.strip() for w in f.readlines()]
+                    wordlist: list[str] = [w.strip() for w in f.readlines()]
             else:
                 raise EnvironmentError(f"Wordlist for {lang} not detected")
 
-        nym = Mnemonym(False, 256, wordlist)
+        nym: Mnemonym = Mnemonym(False, 256, wordlist)
 
         self.assertEqual(
             "..zoo.zoo.zoo.zoo.zoo.zoo.zoo.zoo.zoo.zone",
@@ -87,24 +88,25 @@ class MnemonymTest(unittest.TestCase):
 
     def test_validate_nym(self) -> None:
         with open("../../../test-vectors.json", "r") as fil:
-            vectors = json.load(fil)
+            vectors: dict[str, str] = json.load(fil)
 
         for lang in vectors.keys():
-            path = os.path.join(
+            path: str = os.path.join(
                 os.path.dirname(__file__),
                 f"../../../wordlists/{lang}.txt",
             )
-            wordlist = []
+            wordlist: list[str] = []
 
             if os.path.exists(path) and os.path.isfile(path):
                 with open(path, "r", encoding="utf-8") as f:
-                    wordlist = [w.strip() for w in f.readlines()]
+                    wordlist: list[str] = [w.strip() for w in f.readlines()]
             else:
                 raise EnvironmentError(f"Wordlist for {lang} not detected")
 
         for vec in vectors[lang]:
-            strength = len(bytes.fromhex(vec[0])) * 8
-            nym = Mnemonym(False, strength, wordlist)
+            strength: int = len(bytes.fromhex(vec[0])) * 8
+            nym: Mnemonym = Mnemonym(False, strength, wordlist)
+
             self.assertEqual(
                 True, nym.validate_nym(
                     vec[1]), f"Failed to validate nym {vec[1]}"
